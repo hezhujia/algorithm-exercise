55e5403357225a9621bdd3f843c9ceeb4af776eb
hezhujia <hezhujia@users.noreply.github.com>
2019-11-22 19:56:45 +0800

    提交力扣算法26题-remove-duplicates-from-sorted-array

---
+++ RemoveDuplicates.java
@@ -0,0 +1,16 @@
+public class RemoveDuplicates {
+
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int newArrayLength = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i-1]) {
+                nums[newArrayLength] = nums[i];
+                newArrayLength++;
+            }
+        }
+        return newArrayLength;
+    }
+}
