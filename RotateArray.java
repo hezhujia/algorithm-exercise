bfe62d42a4f2dcf621a3a64cdd22289c46748fe7
hezhujia <hezhujia@users.noreply.github.com>
2019-11-23 19:23:52 +0800

    提交力扣算法189题

---
+++ RotateArray.java
@@ -0,0 +1,17 @@
+public class RotateArray {
+
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverseArray(nums, 0, nums.length-1);
+        reverseArray(nums, 0, k-1);
+        reverseArray(nums, k, nums.length-1);
+    }
+
+    public void reverseArray(int[] nums, int startIndex, int endIndex) {
+        for (int i = startIndex, j = endIndex; i < j; i++, j--) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+}
