0efc839a636d923206388de656387b47d97dffed
hezhujia <hezhujia@users.noreply.github.com>
2019-11-26 22:12:32 +0800

    提交力扣算法42题-trapping-rain-water

---
+++ TrappingRainWater.java
@@ -0,0 +1,30 @@
+public class TrappingRainWater {
+
+    public int trap(int[] height) {
+        int trapedWater = 0;
+        int[] maxLeft = new int[height.length];
+        int[] maxRight = new int[height.length];
+
+        for (int i = 0; i < height.length; i++) {
+            if (i == 0) {
+                maxLeft[i] = height[i];
+            } else {
+                maxLeft[i] = Math.max(maxLeft[i - 1], height[i]);
+            }
+        }
+
+        for (int i = height.length-1; i >= 0 ; i--) {
+            if (i == height.length - 1) {
+                maxRight[i] = height[i];
+            } else {
+                maxRight[i] = Math.max(maxRight[i+1], height[i]);
+            }
+        }
+
+        for (int i = 0; i < height.length; i++) {
+            trapedWater += Math.min(maxLeft[i], maxRight[i]) - height[i];
+        }
+
+        return trapedWater;
+    }
+}
